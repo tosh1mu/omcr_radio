2e372688a6db9c2827f0d356387589c68b69828c
Tosh <1396773+tosh1mu@users.noreply.github.com>
2023-09-11 19:02:44 +0900

    tentative

---
+++ article.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+from dataclasses import dataclass
+from datetime import datetime as dt
+import requests
+from bs4 import BeautifulSoup
+import re
+
+@dataclass(frozen = True, order = True)
+class Episode:
+    id: int
+    title: str
+    pub_date: dt
+    description: str
+    mp3_url: str
+    article_url: str
+
+class Article:
+    def __init__(self, url=""):
+        if len(url) > 0:
+            self.load(url)
+        else:
+            self._id = 0
+            self._title = ""
+            self._pub_date = dt(1900, 1, 1, 0, 0 ,0)
+            self._description = ""
+            self._tags = []
+            self._mp3_urls = []
+            self._url = ""
+    
+    @property
+    def id(self):
+        return self._id
+    
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def pub_date(self):
+        return self._pub_date
+    
+    @property
+    def description(self):
+        return self._description
+    
+    @property
+    def tags(self):
+        return self._tags
+    
+    @property
+    def mp3_urls(self):
+        return self._mp3_urls
+    
+    @property
+    def url(self):
+        return self._url
+    
+    def load(self, url):
+        self._url = url
+        try:
+            res = requests.get(self._url)
+        except requests.exceptions.HTTPError:
+            res.raise_for_status()
+            print(res.text)
+        soup = BeautifulSoup(res.text, "html.parser")
+        self._id = re.findall('[0-9]+', self._url)[0]
+        self._title = soup.select('div[class="title"]')[0].text.strip()
+        tags = soup.select('div[class="tags"]')[0]
+        tag_list = tags.find_all('a')
+        for tag in tag_list:
+            if tag.text != "#ラジオ":
+                self._tags.append(tag)
+        self._pub_date = dt.strptime(soup.select('div[class="date"]')[0].text, '%Y-%m-%d')
+        self._description = soup.select('div[class="description"]')[0].text
+        if soup.find('a', attrs={ 'href': re.compile(r'.*.mp3') }):
+            self._mp3_urls = soup.find_all('a', attrs={ 'href': re.compile(r'.*.mp3') }).get('href')
+        
+        print('Article loaded: ' + self._title + '.')
+        return True
+    
+    def episode_count(self):
+        return len(self._mp3_urls)
+    
+    def episodes(self):
+        episodes = []
+        i = 1
+        for mp3_url in self._mp3_urls:
+            episode = Episode(
+                self._id*10 + i,
+                self._title,
+                self._pub_date,
+                self._description,
+                mp3_url,
+                self._url
+            )
+            episodes.append(episode)
+            i += 1
+        return episodes
--- channel.py
+++ channel.py
@@ -3,9 +3,10 @@
 import requests
 from datetime import datetime as dt
 from bs4 import BeautifulSoup
-import episode
+import article
 import re
 import pandas as pd
+import urllib.parse
 
 from xml.dom.minidom import parseString
 
@@ -18,20 +19,21 @@ class Channel:
         self._description = description
         self._img_href = img_href
         self._channel_url = channel_url
-        self._episode_dict = {}
+        self._article_dict = {}
         self._load_list_datetime = dt(1900, 1, 1, 0, 0, 0)
-        self._load_episodes_datetime = dt(1900, 1, 1, 0, 0, 0)
+        self._load_articles_datetime = dt(1900, 1, 1, 0, 0, 0)
+        self._episodes = []
         self._master_csv = "csv/" + self._abbreviation + "_master.csv"
         self._rss_file = "docs/" + self._abbreviation + ".rss"
     
-    # ラジオページからエピソードタイトルとURLのリストを読み込む
-    def get_episode_list(self):
+    # ラジオページから記事タイトルとURLのリストを読み込む
+    def get_article_list(self):
         self._load_list_datetime = dt.now()
         page = 1
         flag = True
         while flag is True:
             url = self._channel_url + str(page) + "/"
-            print("Getting episode list from " + url + "...")
+            print("Getting episode list from " + urllib.parse.unquote(url) + "...")
             try:
                 res = requests.get(url)
             except requests.exceptions.HTTPError:
@@ -48,104 +50,113 @@ class Channel:
                 if len(links) < 2:
                     continue
                 else:
-                    radio_page_url = links[0].get('href')
-                    radio_title = links[1].text
-                    self._episode_dict[radio_title] = radio_page_url
+                    article_url = links[0].get('href')
+                    article_title = links[1].text
+                    self._article_dict[article_title] = article_url
             page += 1
 
         return True
+    
+    # 読み込み済みの記事かどうかを判別する
+    def check_article_duplication(self, url):
+        duplication = False
+        article_urls = []
+        for ep in self._episodes:
+            article_urls.append(ep.article_url)
+        if url in article_urls:
+            duplication = True
 
-    # 全てのエピソードを取得
-    def get_all_episodes(self):
-        self._load_episodes_datetime = dt.now()
+        return duplication
+    
+    # 読み込み済みのエピソードかどうかを判別する
+    def check_episode_duplication(self, episode):
+        duplication = False
+        if episode in self._episodes:
+            duplication = True
+        return duplication
+    
+    # 記事ページを読み込み
+    def load_article(self, url):
+        atcl = article.Article(url)
+        for ep in atcl.episodes():
+            if self.check_episode_duplication(ep) is False:
+                self._episodes.append(ep)
+        
+        return True
+    
+    # CSVから読み込み
+    def load_csv(self):
+        eps = pd.read_csv(self._master_csv, encoding='utf8', index_col=0)
 
+        for id, title, pub_date, description, mp3_url, article_url in zip(eps.index, eps['title'], eps['pub_date'], eps['description'], eps['mp3_url'], eps['article_url']):
+            ep = article.Episode(id, title, pub_date, description, mp3_url, article_url)
+            self._episodes.append(ep)
+
+        return True
+    
+    # CSVに書き込み
+    def save_csv(self):
         ids = []
         channels = []
         titles = []
         descriptions = []
         pub_dates = []
         mp3_urls = []
-        episode_urls = []
+        article_urls = []
 
-        self.get_episode_list()
+        self._episodes.sort()
 
-        for title, url in self._episode_dict.items():
-            ep = episode.Episode(url)
-            ep_data = ep.data()
-            print("Loading " + ep_data.title)
-            ids.append(ep_data.id)
+        for ep in self._episodes:
+            ids.append(ep.id)
             channels.append(self._channel_title)
-            titles.append(ep_data.title)
-            descriptions.append(ep_data.description)
-            pub_dates.append(ep_data.pub_date)
-            mp3_urls.append(ep_data.mp3_url)
-            episode_urls.append(url)
-
-        all_episodes = pd.DataFrame(
+            titles.append(ep.title)
+            descriptions.append(ep.description)
+            pub_dates.append(ep.pub_date)
+            mp3_urls.append(ep.mp3_url)
+            article_urls.append(ep.url)
+                            
+        episodes_df = pd.DataFrame(
             {'channel': channels,
              'title': titles,
              'description': descriptions,
              'pub_date': pub_dates,
              'mp3_url': mp3_urls,
-             'episode_url': episode_urls},
+             'article_url': article_urls},
              index = ids
         )
 
-        all_episodes.sort_values('pub_date')
-        all_episodes.to_csv(self._master_csv)
+        episodes_df.sort_values('pub_date')
+        episodes_df.to_csv(self._master_csv)
+
+        return True
+    
+    # 全記事を読み込み直す
+    def refresh(self):
+        self._article_dict.clear()
+        self.get_article_list()
+        self._episodes.clear()
+        for title, url in self._article_dict.items():
+            self.load_article(url)
         
+        self.save_csv()
         return True
 
-    # 新しいエピソードを取得
-    def get_new_episodes(self):
-        existing_episodes = pd.read_csv(self._master_csv, encoding='utf8', index_col=0)
-        self.get_episode_list()
+    # まだ読み込んでいない記事のみ読み込む
+    def update(self):
+        self._article_dict.clear()
+        self.get_article_list()
+        self._episodes.clear()
+        self.load_csv()
 
-        new_ids = []
-        new_channels = []
-        new_titles = []
-        new_descriptions = []
-        new_pub_dates = []
-        new_mp3_urls = []
-        new_episode_urls = []
-
-        for title, url in self._episode_dict.items():
-            if url in existing_episodes['episode_url'].values:
-                continue
-            else:
-                new_ep = episode.Episode(url)
-                new_ep_data = new_ep.data()
-                print("Loading " + new_ep_data.title)
-                new_ids.append(new_ep_data.id)
-                new_channels.append(self._channel_title)
-                new_titles.append(new_ep_data.title)
-                new_descriptions.append(new_ep_data.description)
-                new_pub_dates.append(new_ep_data.pub_date.strftime('%Y-%m-%d'))
-                new_mp3_urls.append(new_ep_data.mp3_url)
-                new_episode_urls.append(url)
+        for title, url in self._article_dict.items():
+            if self.check_article_duplication(url) is False:
+                self.load_article(url)
         
-        updated_episodes = existing_episodes
-
-        if len(new_ids) > 0:
-            new_episodes = pd.DataFrame(
-                {'channel': new_channels,
-                 'title': new_titles,
-                 'description': new_descriptions,
-                 'pub_date': new_pub_dates,
-                 'mp3_url': new_mp3_urls,
-                 'episode_url': new_episode_urls},
-                 index = new_ids
-            )
-            updated_episodes = pd.concat([existing_episodes, new_episodes])
-        
-        updated_episodes.sort_values('pub_date')
-        updated_episodes.to_csv(self._master_csv)
-
+        self.save_csv()
         return True
     
     # rssに書き出す
     def make_rss(self):
-        episodes = pd.read_csv(self._master_csv, encoding='utf8', index_col=0)
         xml_template = "<rss version=\"2.0\" xmlns:itunes=\"http://www.itunes.com/dtds/podcast-1.0.dtd\">\
                             <channel>\
                                 <title>title</title>\
@@ -170,27 +181,27 @@ class Channel:
         channel.getElementsByTagName("itunes:image")[0].attributes["href"].value = self._img_href
         channel.getElementsByTagName("link")[0].firstChild.data = self._channel_url
 
-        for title, description, pub_date, mp3_url, episode_url in zip(episodes['title'], episodes['description'], episodes['pub_date'], episodes['mp3_url'], episodes['episode_url']):
+        for episode in self._episodes:
             item = dom.createElement('item')
             channel.appendChild(item)
             # タイトルの設定
             title_node = dom.createElement('title')
-            title_node.appendChild(dom.createTextNode(title))
+            title_node.appendChild(dom.createTextNode(episode.title))
             item.appendChild(title_node)
             # 説明文の設定
             description_node = dom.createElement('description')
-            description_node.appendChild(dom.createTextNode(str(description)))
+            description_node.appendChild(dom.createTextNode(str(episode.description)))
             item.appendChild(description_node)
             # 公開日の設定
             pubdate_node = dom.createElement('pubDate')
-            pubdate = dt.strptime(pub_date, '%Y-%m-%d')
+            pubdate = dt.strptime(episode.pub_date, '%Y-%m-%d')
             pubdate_str = pubdate.strftime('%a, %d %b %Y %H:%M:%S +0900')
             pubdate_node.appendChild(dom.createTextNode(pubdate_str))
             item.appendChild(pubdate_node)
             # mp3のURL
             enclosure_node = dom.createElement('enclosure')
             url_attr = dom.createAttribute('url')
-            url_attr.value = str(mp3_url)
+            url_attr.value = str(episode.mp3_url)
             enclosure_node.setAttributeNode(url_attr)
             type_attr = dom.createAttribute('type')
             type_attr.value = "audio/mpeg"
@@ -198,7 +209,7 @@ class Channel:
             item.appendChild(enclosure_node)
             # GUID(記事URL)
             guid_node = dom.createElement('guid')
-            guid_node.appendChild(dom.createTextNode(str(episode_url)))
+            guid_node.appendChild(dom.createTextNode(str(episode.article_url)))
             is_link_attr = dom.createAttribute('isPermaLink')
             is_link_attr.value = "True"
             guid_node.setAttributeNode(is_link_attr)
