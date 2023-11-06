127d064cdf203ffb341484f409255a18732d85b3
Tosh <1396773+tosh1mu@users.noreply.github.com>
2023-11-06 19:47:08 +0900

    updated

--- main.py
+++ main.py
@@ -91,5 +91,5 @@ shimowata_channel = channel.Channel(
     "https://omocoro.jp/tag/%e3%82%b7%e3%83%a2%e3%83%80%e3%83%86%e3%83%84%e3%83%a4%e3%81%a8%e7%a7%81%ef%bc%88%e3%83%a8%e3%83%83%e3%83%94%e3%83%bc%ef%bc%89/page/"
 )
 # shimowata_channel.refresh()
-shimowata_channel.update()
-shimowata_channel.make_rss()
+# shimowata_channel.update()
+# shimowata_channel.make_rss()
