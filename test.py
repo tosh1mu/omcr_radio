78217b0ef4f2072c647e40b9bb34125bbc91a7ed
Tosh <1396773+tosh1mu@users.noreply.github.com>
2023-11-29 21:21:57 +0900

    tentative

--- test.py
+++ test.py
@@ -2,20 +2,7 @@
 import radio_page as rp
 import pandas as pd
 
-test_url = "https://omocoro.jp/radio/396342/"
-
-test_page = rp.RadioPage(test_url)
-
-pd.to_pickle(test_page, "test.pkl")
-
-read_test = pd.read_pickle("test.pkl")
-print(read_test.mp3_urls)
-
 channel_df = pd.read_excel('csv/omcr_radio.xlsx', sheet_name='channel', index_col=0)
-print(channel_df)
-
 radiopage_df = pd.read_excel('csv/omcr_radio.xlsx', sheet_name='radio_page', index_col=0)
-print(radiopage_df)
-
 
 
