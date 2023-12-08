7c66d3945d372024c95395c25ef9984fad329222
Tosh <1396773+tosh1mu@users.noreply.github.com>
2023-12-08 12:22:21 +0900

    tentative

--- test.py
+++ test.py
@@ -12,5 +12,3 @@ import omcr
 # channel_df = pd.read_excel('csv/omcr_radio.xlsx', sheet_name='channel', index_col=0)
 # radiopage_df = pd.read_excel('csv/omcr_radio.xlsx', sheet_name='radio_page', index_col=0)
 
-tokumei = omcr.TagHandler('ニュース！オモコロウォッチ！')
-tokumei.refresh()
