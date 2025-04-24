a49219cb6a4b1bc26876296fc2a2a213ba2d514b
tosh <nello55r30@pm.me>
2025-04-24 13:19:25 +0900

    updated

--- omcr.py
+++ omcr.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from functools import total_ordering
 from datetime import datetime as dt
 import datetime
+from typing import List, Tuple, Optional
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -10,6 +11,19 @@ import urllib.parse
 import hashlib
 import pandas as pd
 import os
+from pathlib import Path
+
+class OMCRError(Exception):
+    """オモコロラジオの基本例外クラス"""
+    pass
+
+class InvalidURLError(OMCRError):
+    """無効なURLが指定された場合の例外"""
+    pass
+
+class FetchError(OMCRError):
+    """データ取得に失敗した場合の例外"""
+    pass
 
 ## エピソードを扱うクラス
 @dataclass(frozen = True, order = True)
@@ -23,15 +37,15 @@ class Episode:
 ## オモコロラジオの記事ページを扱うクラス
 @total_ordering
 class RadioPage:
-    def __init__(self, url=""):
-        self._url = ""
-        self._id = 0
-        self._title = ""
-        self._pub_date = dt(1900, 1, 1, 0, 0 ,0)
-        self._description = ""
-        self._tags = []
-        self._mp3_urls = []
-        if len(url) > 0:
+    def __init__(self, url: str = "") -> None:
+        self._url: str = ""
+        self._id: int = 0
+        self._title: str = ""
+        self._pub_date: dt = dt(1900, 1, 1, 0, 0 ,0)
+        self._description: str = ""
+        self._tags: List[str] = []
+        self._mp3_urls: List[str] = []
+        if url:
             self.set_url(url)
     
     def __eq__(self, other):
@@ -73,69 +87,58 @@ class RadioPage:
         return self._mp3_urls
 
     ## オモコロのリンクかどうかを確認する
-    def check_url(self, url):
-        res = re.match(r'^(https://omocoro.jp/).*[0-9]+', url)
-        if res:
-            return True
-        else:
-            return False
+    def check_url(self, url: str) -> bool:
+        return bool(re.match(r'^(https://omocoro.jp/).*[0-9]+', url))
     
     ## URLを設定し、ページの内容を読み込む
-    def set_url(self, url):
-        genuine = self.check_url(url)
-        if genuine:
-            self._url = url
-            self.load_page()
-            return True
-        else:
-            print('Invalid URL: ' + url)
-            return False
+    def set_url(self, url: str) -> bool:
+        if not self.check_url(url):
+            raise InvalidURLError(f'Invalid URL: {url}')
+        self._url = url
+        return self.load_page()
 
     ## ラジオページの内容を読み込む
-    def load_page(self):
+    def load_page(self) -> bool:
         try:
             res = requests.get(self._url)
-        except requests.exceptions.HTTPError:
             res.raise_for_status()
-            print(res.text)
+        except requests.exceptions.RequestException as e:
+            raise FetchError(f'Failed to fetch page: {str(e)}')
+
         soup = BeautifulSoup(res.text, "html.parser")
-        self._id = int(re.findall('[0-9]+', self._url)[0])
-        self._title = str(soup.select('div[class="title"]')[0].text.strip())
-        tags = soup.select('div[class="tags"]')[0]
-        tag_list = tags.find_all('a')
-        for tag in tag_list:
-            self._tags.append(str(tag))
-        self._pub_date = dt.strptime(soup.select('div[class="date"]')[0].text, '%Y-%m-%d')
-        self._description = str(soup.select('div[class="description"]')[0].text)
-        if soup.find('a', attrs={ 'href': re.compile(r'.*.mp3') }):
-            for result in soup.find_all('a', attrs={ 'href': re.compile(r'.*.mp3') }):
-                mp3_url = str(result.get('href'))
-                if mp3_url not in self._mp3_urls:
-                    self._mp3_urls.append(mp3_url)
-        
-        print(str(len(self._mp3_urls)) + ' mp3(s) found from ' + self._title + '.')
-        return True
+        try:
+            self._id = int(re.findall('[0-9]+', self._url)[0])
+            self._title = soup.select_one('div[class="title"]').text.strip()
+            tags = soup.select_one('div[class="tags"]')
+            self._tags = [str(tag) for tag in tags.find_all('a')]
+            self._pub_date = dt.strptime(soup.select_one('div[class="date"]').text, '%Y-%m-%d')
+            self._description = soup.select_one('div[class="description"]').text
+
+            mp3_links = soup.find_all('a', attrs={'href': re.compile(r'.*.mp3')})
+            self._mp3_urls = [str(link.get('href')) for link in mp3_links]
+            
+            print(f'{len(self._mp3_urls)} mp3(s) found from {self._title}.')
+            return True
+        except (AttributeError, IndexError) as e:
+            raise FetchError(f'Failed to parse page content: {str(e)}')
 
     ## ラジオページに含まれるエピソードをリストで返す
-    def get_episodes(self):
-        episode_list = []
-        i = 1
-        for mp3_url in self._mp3_urls:
-            episode = Episode(
+    def get_episodes(self) -> List[Episode]:
+        return [
+            Episode(
                 self._title,
-                self._pub_date + datetime.timedelta(seconds=i),
+                self._pub_date + datetime.timedelta(seconds=i+1),
                 self._description,
                 mp3_url,
                 self._url
             )
-            episode_list.append(episode)
-            i += 1
-        return episode_list
+            for i, mp3_url in enumerate(self._mp3_urls)
+        ]
     
 ## 記事リストページを扱うクラス
 class ArticleList:
-    def __init__(self, base_url, page = 1, sort = "new"):
-        self._page_url = base_url + "page/" + str(page) + "/?sort=" + sort
+    def __init__(self, base_url: str, page: int = 1, sort: str = "new") -> None:
+        self._page_url = f"{base_url}page/{page}/?sort={sort}"
         self._links = self.get_links() # 記事のタイトル：URL
     
     @property
@@ -147,37 +150,45 @@ class ArticleList:
         return self._links
     
     ## 記事リストページ内にある記事URLを取得
-    def get_links(self):
+    def get_links(self) -> List[Tuple[str, dt, str, str]]:
         links = []
         try:
             res = requests.get(self._page_url)
-        except requests.exceptions.HTTPError:
             res.raise_for_status()
-            print(res.text)
-            return links
+        except requests.exceptions.RequestException as e:
+            raise FetchError(f'Failed to fetch article list: {str(e)}')
+
         soup = BeautifulSoup(res.text, "html.parser")
         boxes = soup.select('div[class="box"]')
-        if len(boxes) > 0:
-            for box in boxes:
-                category = box.select_one('div[class="category"]').select_one("span").text
+        
+        for box in boxes:
+            try:
+                category = box.select_one('div[class="category"] span').text
                 if category != "ラジオ":
                     continue
+                    
                 date = dt.strptime(box.select_one('div[class="date"]').text, '%Y.%m.%d')
-                title = box.select_one('div[class="title"]').select_one("a").text
-                link = box.select_one('div[class="title"]').find('a').get('href')
-                if link[-1] != "/":
-                    link = link + "/"
+                title = box.select_one('div[class="title"] a').text
+                link = box.select_one('div[class="title"] a').get('href')
+                
+                if not link.endswith("/"):
+                    link += "/"
+                    
                 links.append((category, date, title, link))
-        print(str(len(links)) + ' link(s) found from ' + urllib.parse.unquote(self._page_url) + '.')
+            except (AttributeError, ValueError) as e:
+                print(f'Warning: Failed to parse box content: {str(e)}')
+                continue
+
+        print(f'{len(links)} link(s) found from {urllib.parse.unquote(self._page_url)}.')
         return links
 
 ## 指定タグを扱うクラス
 class TagHandler:
-    def __init__(self, tag):
+    def __init__(self, tag: str) -> None:
         self._tag = tag
-        self._pickle_name = str(hashlib.md5(self._tag.encode()).hexdigest()) + '.pkl'
-        self._base_url = 'https://omocoro.jp/tag/' + self._tag + '/'
-        self._articles = []
+        self._pickle_name = f"{hashlib.md5(self._tag.encode()).hexdigest()}.pkl"
+        self._base_url = f'https://omocoro.jp/tag/{self._tag}/'
+        self._articles: List[RadioPage] = []
 
     @property    
     def pickle_name(self):
@@ -191,28 +202,28 @@ class TagHandler:
         self._articles = sorted(self._articles)
         return True
     
-    def check_url(self, url):
-        exist = False
-        for article in self._articles:
-            if article.url == url:
-                exist = True
-        return exist
+    def check_url(self, url: str) -> bool:
+        return any(article.url == url for article in self._articles)
 
     ## 指定のページ内にある記事を取得する
-    def get(self, page = 1, sort = "new"):
+    def get(self, page: int = 1, sort: str = "new") -> Tuple[int, int]:
         article_list = ArticleList(self._base_url, page, sort)
         links = article_list.links
         add_count = 0
-        if len(links) < 1:
+
+        if not links:
             return (0, 0)
-        for link in links:
-            title = link[2]
-            url = link[3]
-            exist = self.check_url(url)
-            if not exist:
-                radio_page = RadioPage(link[3])
-                self._articles.append(radio_page)
-                add_count += 1
+
+        for _, _, _, url in links:
+            if not self.check_url(url):
+                try:
+                    radio_page = RadioPage(url)
+                    self._articles.append(radio_page)
+                    add_count += 1
+                except (InvalidURLError, FetchError) as e:
+                    print(f'Warning: Failed to process {url}: {str(e)}')
+                    continue
+
         return (len(links), add_count)
     
     ## ページ数を指定して、記事を取得する
@@ -253,17 +264,16 @@ class TagHandler:
         self.update()
         return True
     
-    def save(self, dir = "pickles/"):
-        pickle_path = dir + self._pickle_name
+    def save(self, dir: str = "pickles/") -> bool:
+        pickle_path = Path(dir) / self._pickle_name
+        pickle_path.parent.mkdir(parents=True, exist_ok=True)
         pd.to_pickle(self, pickle_path)
         return True
     
-    def load(self, dir = "pickles/"):
-        pickle_path = dir + self._pickle_name
-        pickle_exist = os.path.isfile(pickle_path)
-        if pickle_exist is True:
+    def load(self, dir: str = "pickles/") -> bool:
+        pickle_path = Path(dir) / self._pickle_name
+        if pickle_path.is_file():
             loaded = pd.read_pickle(pickle_path)
             self._articles = loaded.articles
             return True
-        else:
-            return False
+        return False
